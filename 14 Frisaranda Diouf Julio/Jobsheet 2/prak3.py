6c1bc83c0db00ef4aacce79923cd7fb003c0b588
Frisaranda Diouf Julio <frisaranda@gmail.com>
2023-09-12 12:15:04 +0700

    submit js2

---
+++ 14 Frisaranda Diouf Julio/Jobsheet 2/prak3.py
@@ -0,0 +1,122 @@
+# RANDOM SPLIT
+# LANGKAH 1 - Load Data
+import pandas as pd
+
+df = pd.read_csv('Jobsheet 2/dataset/Titanic-Dataset-selected.csv')
+print(df.head())
+
+
+# LANGKAH 2 - Split Data
+# Split data
+from sklearn.model_selection import train_test_split
+
+# Split data training dan dan lainnya
+# data lainnya, akan kita split lagi menjadi validasi dan testing.
+# Rasio yang akan kita gunakan adalah 8:1:1
+df_train, df_unseen = train_test_split(df, test_size=0.2, random_state=0)
+
+# Split lagi antara validasi dan testing
+df_val, df_test = train_test_split(df_unseen, test_size=0.5, random_state=0)
+
+# Cek masing-masing ukuran data
+
+print(f'Jumlah data asli: {df.shape[0]}')
+print(f'Jumlah data train: {df_train.shape[0]}')
+print(f'Jumlah data val: {df_val.shape[0]}')
+print(f'Jumlah data test: {df_test.shape[0]}')
+
+# Cek rasio tiap label
+print('=========')
+print(f'Jumlah label data asli:\n{df.Survived.value_counts()}')
+print(f'Jumlah label data train:\n{df_train.Survived.value_counts()}')
+print(f'Jumlah label data val:\n{df_val.Survived.value_counts()}')
+print(f'Jumlah label data test:\n{df_test.Survived.value_counts()}')
+
+
+
+# STRATIFIED SPLIT
+# LANGKAH 1 - Load Data
+import pandas as pd
+
+df2 = pd.read_csv('Week 2/Titanic-Dataset-selected.csv')
+print(df2.head())
+
+
+# LANGKAH 2 - Split Data
+# Split data
+from sklearn.model_selection import train_test_split
+
+# Split data training dan dan lainnya
+# data lainnya, akan kita split lagi menjadi validasi dan testing.
+# Rasio yang akan kita gunakan adalah 8:1:1
+df2_train, df2_unseen = train_test_split(df2, test_size=0.2, random_state=0, stratify=df2['Survived'])
+
+# Split lagi antara validasi dan testing
+df2_val, df2_test = train_test_split(df2_unseen, test_size=0.5, random_state=0, stratify=df2_unseen['Survived'])
+
+# Cek masing-masing ukuran data
+
+print(f'Jumlah label data asli:\n{df2.Survived.value_counts()}')
+print(f'Jumlah label data train:\n{df2_train.Survived.value_counts()}')
+print(f'Jumlah label data val:\n{df2_val.Survived.value_counts()}')
+print(f'Jumlah label data test:\n{df2_test.Survived.value_counts()}')
+
+
+
+# CROSS VALIDATION 1
+# LANGKAH 1 - Load Data
+import pandas as pd
+
+df3 = pd.read_csv('Week 2/Titanic-Dataset-selected.csv')
+print(df3.head())
+
+
+# LANGKAH 2 - Split Data
+# Implementasi k-fold cross validation (random) dengan training dan testing saja
+from sklearn.model_selection import KFold
+
+# inisiasi obyek kfold
+kf = KFold(n_splits=4)
+print(f'Jumlah fold: {kf.get_n_splits()}')
+print(f'Obyek KFold: {kf}')
+
+# Lakukan splitting dengan KFold
+kf_split = kf.split(df3)
+print(f'Jumlah data df: {df.shape[0]}')
+
+# cek index data tiap fold
+for train_index, test_index in kf_split:
+    print(f'Index train: {train_index} | Index test: {test_index}')
+
+
+
+# CROSS VALIDATION 2
+# LANGKAH 1 - Load Data
+import pandas as pd
+
+df4 = pd.read_csv('Week 2/Titanic-Dataset-selected.csv')
+print(df4.head())
+
+
+# LANGKAH 2 - Split Data
+# Implementasi k-fold cross validation (random) dengan training, validation, dan testing data
+from sklearn.model_selection import train_test_split, KFold
+
+# Split dulu antara data training dan testing dengan train_test_split
+# Rasio 8:2 untuk training dan testing
+df4_train, df4_test = train_test_split(df4, test_size=0.2, random_state=0)
+
+# inisiasi obyek kfold
+kf2 = KFold(n_splits=4)
+print(f'Jumlah fold: {kf2.get_n_splits()}')
+print(f'Obyek KFold: {kf2}')
+
+# Lakukan splitting dengan KFold untuk data df_training
+# Dengan acara ini, kita masih memiliki data testing untuk keperluan pengujian model
+# namun tetap dapat melakukan evaluasi dengan menggunakan data validasi
+kf2_split = kf2.split(df_train)
+print(f'Jumlah data df_train: {df4_train.shape[0]}')
+
+# cek index data tiap fold
+for train_index, test_index in kf2_split:
+    print(f'Index train: {train_index} | Index test: {test_index}')
